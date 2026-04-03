7abdf61f97260c82c3069ddefaf2bdc9a6cbbe41
Mani Kumar <mani.malle.dev@gmail.com>
2026-04-03 20:21:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <h1>Welcome to React</h1>
+        <p>This is a simple React app.</p>
+      </div>
+    );
   }
 }
 export default App;
